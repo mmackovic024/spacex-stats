2f2b9f173decf2b998d0448c9eecd9b702ed6277
mmackovic024 <mackovic.mirko@gmail.com>
2019-05-03 16:35:20 +0200

    added static folder

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
-//const bodyParser = require('body-parser');
 const graphqlHTTP = require('express-graphql');
 const cors = require('cors');
 const schema = require('./schemaB');
 const resolvers = require('./resolversB');
+const path = require('path');
 
 const app = express();
-
-//app.use(bodyParser.json());
 
 // Allow cross-origin
 app.use(cors());
@@ -20,6 +18,12 @@ app.use(
     graphiql: true
   })
 );
+
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
