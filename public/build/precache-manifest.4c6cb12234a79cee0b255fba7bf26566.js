319796e1697b42ea57b1ae204d6b4e79aaa33f37
mmackovic024 <mackovic.mirko@gmail.com>
2019-06-02 09:33:41 +0200

    rebuild

---
+++ public/build/precache-manifest.4c6cb12234a79cee0b255fba7bf26566.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abfa9f1225d43b22ecde00e79ee864eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0511d04ac4d42c28d2d",
+    "url": "/static/css/main.e23fee82.chunk.css"
+  },
+  {
+    "revision": "de5c11e337876961e14f",
+    "url": "/static/js/2.f0af2b42.chunk.js"
+  },
+  {
+    "revision": "b0511d04ac4d42c28d2d",
+    "url": "/static/js/main.b99467f5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
